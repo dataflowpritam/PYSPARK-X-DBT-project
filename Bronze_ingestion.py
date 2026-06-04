41f46c38e3a1bc9b72780fed72df3b46c184e252
PRITAM_SARKAR <sarkarpritam9089@gmail.com>
2026-06-05 03:10:15 +0530

    Add files via upload

---
+++ Bronze_ingestion.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### spark streaming
+
+# COMMAND ----------
+
+entities = ['customers', 'drivers', 'locations', 'payments', 'trips', 'vehicles']
+
+# COMMAND ----------
+
+for entity in entities:
+
+    df_batch = spark.read.format("csv")\
+        .option("header", True)\
+        .option("inferSchema", True)\
+        .load(f"/Volumes/pysparkdbt/source/source_data/{entity}/")
+
+    schema_entity = df_batch.schema
+
+    df = spark.readStream.format('csv')\
+        .option('header', True)\
+        .schema(schema_entity)\
+        .load(f"/Volumes/pysparkdbt/source/source_data/{entity}/")
+
+
+    df.writeStream.format('delta')\
+            .outputMode('append')\
+            .option('checkpointLocation', f'/Volumes/pysparkdbt/bronze/checkpoint/{entity}')\
+            .trigger(once=True)\
+            .toTable(f'pysparkdbt.bronze.{entity}')   
+
+
+
